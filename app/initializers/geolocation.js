5e3e576b82f12b0241bbdb9db29b2761e8fa4c1e
nem035 <n.stojanovic035@gmail.com>
2016-12-17 06:22:15 -0500

    geolocation fail-safe

--- app/initializers/geolocation.js
+++ app/initializers/geolocation.js
@@ -6,36 +6,42 @@ export function initialize(application) {
   // defer application booting until we obtain geolocation data
   application.deferReadiness();
 
-  // request geolocation data
-  navigator.geolocation.getCurrentPosition(({ coords }) => {
-    const {
-      latitude: lat,
-      longitude: lng
-    } = coords;
+  if ('geolocation' in navigator) {
 
-    // we got the geolocation data, application is ready to continue booting
-    application.advanceReadiness();
+    // request geolocation data
+    navigator.geolocation.getCurrentPosition(({ coords }) => {
+      const {
+        latitude: lat,
+        longitude: lng
+      } = coords;
 
-    // log information if we're in debug environment
-    Ember.runInDebug(() => {
-      Ember.Logger.info(
-        `registering "data:location": { lat: ${lat}, lng: ${lng}`
-      );
+      // we got the geolocation data, application is ready to continue booting
+      application.advanceReadiness();
+
+      // log information if we're in debug environment
+      Ember.runInDebug(() => {
+        Ember.Logger.info(
+          `registering "data:location": { lat: ${lat}, lng: ${lng}`
+        );
+      });
+
+      // register geolocation data
+      // can be accessed from any factory instance using Ember.getOwner(this).lookup('data:location')
+      application.register('data:location', {
+        lat,
+        lng
+      }, {
+        instantiate: false
+      });
+
+      // inject geolocation data into all all factories of the type 'controller'
+      // with the name 'location'
+      application.inject('controller', 'location', 'data:location');
     });
-
-    // register geolocation data
-    // can be accessed from any factory instance using Ember.getOwner(this).lookup('data:location')
-    application.register('data:location', {
-      lat,
-      lng
-    }, {
-      instantiate: false
-    });
-
-    // inject geolocation data into all all factories of the type 'controller'
-    // with the name 'location'
-    application.inject('controller', 'location', 'data:location');
-  });
+  } else {
+    /* geolocation IS NOT available */
+  }
+  
 
 }
 
