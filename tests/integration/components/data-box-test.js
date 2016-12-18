9ed35386074b9816c33d87d399ccda86e78b4199
nem035 <n.stojanovic035@gmail.com>
2016-12-18 01:32:28 -0500

    fixed a test bug

--- tests/integration/components/data-box-test.js
+++ tests/integration/components/data-box-test.js
@@ -1,25 +1,33 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import {
+  moduleForComponent,
+  test,
+} from 'ember-qunit';
+
 import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('data-box', 'Integration | Component | data box', {
   integration: true
 });
 
-test('it renders', function(assert) {
+test('it renders', function (assert) {
 
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{data-box}}`);
+  this.render(hbs `{{data-box}}`);
 
-  assert.equal(this.$().text().trim(), '');
+  assert.equal(this.$().text().trim(), 'N/A');
+
+  this.render(hbs `{{data-box data="test"}}`);
+
+  assert.equal(this.$().text().trim(), 'test');
 
   // Template block usage:
-  this.render(hbs`
+  this.render(hbs `
     {{#data-box}}
       template block text
     {{/data-box}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), 'N/A');
 });
